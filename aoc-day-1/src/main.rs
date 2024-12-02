eb7bde722d63c5c331eddd71d12d6d5c6bedc065
Marcus Maciel Oliveira <marq938@gmail.com>
2024-12-01 21:44:30 -0300

    jogando cargo do shell

---
+++ aoc-day-1/src/main.rs
@@ -0,0 +1,33 @@
+fn read_lines(filename: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut lista_1: Vec<i32> = Vec::new();
+    let mut lista_2: Vec<i32> = Vec::new();
+
+    for line in std::fs::read_to_string(filename).unwrap().lines() {
+        let split_line: Vec<String> = line.split("   ").map( |l| l.to_string() ).collect();
+        lista_1.push(split_line[0].parse::<i32>().unwrap());
+        lista_2.push(split_line[1].parse::<i32>().unwrap());
+    }
+
+    return (lista_1, lista_2);
+}
+
+fn main() {
+    // primeiro passo: parse nos inputs e converter ambos em listas
+    println!("Hello, world!");
+    let (mut lista_1, mut lista_2) = read_lines("resources/lista_numeracoes.txt");
+    // segundo passo: ordenar as listas
+    lista_1.sort();
+    lista_2.sort();
+    // terceiro passo: comparar os numeros e receber a diferenca entre os menores numeros;
+    let mut lista_dif = Vec::new();
+    for i in 0..lista_1.len() {
+        lista_dif.push((lista_1[i] - lista_2[i]).abs());
+    }
+    // quarto e ultimo passo: somar elementos do vetor
+    let mut sum = 0;
+    for elemento in lista_dif {
+        sum += elemento
+    }
+    println!("{}", sum)
+
+}
